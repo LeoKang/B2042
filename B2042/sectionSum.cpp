aaeb9796fa01adde3cad55c8f5aba43a2e25b41f
KSJNOTE <KSJNOTE@KSJNOTE-PC>
2017-07-19 20:29:39 +0900

    init

---
+++ B2042/sectionSum.cpp
@@ -0,0 +1,18 @@
+/*
+ * sectionSum.cpp
+ *
+ *  Created on: 2017. 7. 19.
+ *      Author: KSJNOTE
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "test" << endl;
+
+	return 0;
+}
+
+
